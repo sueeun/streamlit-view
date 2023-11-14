e8e708b6ff407844ae61529a19105228226ce5c1
sueeun <83956730+sueeun@users.noreply.github.com>
2023-11-14 14:46:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,48 @@
 import streamlit as st
 import pandas as pd
+from datetime import datetime
 
-view = [100, 150, 30]
-st.write('# Youtube view')
-st.write('## raw')
-view
-st.write('## bar chart')
-st.bar_chart(view)
+def process_log_data(log_df):
+    log_df.drop(columns='timestamp', inplace=True)
+    log_df['Timestamp'] = log_df['message'].str.extract(r'(\d+/\w+/\d+\d+:\d+:\d+:\d+)')
+    log_df['Timestamp'] = pd.to_datetime(log_df['Timestamp'], format='%d/%b/%Y:%H:%M:%S').dt.strftime('%Y-%m-%d %H:%M:%S')
+    log_df['Host'] = log_df['message'].str.extract(r'(\d+.\d+.\d+.\d+)')
+    log_df[['Method', 'Path']] = log_df['message'].str.extract(r'(HEAD|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH|POST|GET)\s+(.?)\s+HTTP')
+    log_df['Protocol'] = log_df['message'].str.extract(r'(HTTP/\d+.\d+)')
+    log_df['Status'] = log_df['message'].str.extract(r'(\d+)\s+\d+')
+    log_df['Bytes'] = log_df['message'].str.extract(r'\d+\s+(\d+)')
+    log_df['UA'] = log_df['message'].str.extract(r'(Mozilla.+537.36)')
+    selected_log_df = log_df[log_df['Method'].isna() & log_df['Protocol'].isna()]
+    log_df['Payload'] = selected_log_df['message'].str.extract(r']{1}\s+"(.)" \d+')
+    log_df['Referer'] = log_df['message'].str.extract(r'."(http[s]?://.?)"')
+    log_df.drop(columns='message', inplace=True)
+    log_df = log_df[['Timestamp','Method','Protocol','Status','Referer','Path','Host','UA','Payload','Bytes']]
+    return log_df
 
-sview = pd.Series(view)
-sview
+def main():
+    st.title('로그 데이터 처리 앱')
+
+    # 파일 업로드
+    uploaded_file = st.file_uploader("CSV 파일 선택", type="csv")
+
+    if uploaded_file is not None:
+        # CSV 파일 읽기
+        log_df = pd.read_csv(uploaded_file)
+
+        # 로그 데이터 처리
+        processed_log_df = process_log_data(log_df)
+
+        # 처리된 데이터 표시
+        st.write("처리된 로그 데이터:")
+        st.write(processed_log_df)
+
+        # 처리된 데이터를 새로운 CSV 파일로 저장
+        processed_file_path = 'processed_file.csv'
+        processed_log_df.to_csv(processed_file_path, index=False)
+
+        # 처리된 파일을 다운로드할 수 있는 링크 제공
+        st.markdown(f"처리된 데이터 다운로드: [처리된 파일]({processed_file_path})")
+
+
+if __name__ == '__main__':
+    main()
