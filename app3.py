d91c74f1e7da6ccaa9fcf3f35ca9dd346dbc3c08
sueeun <83956730+sueeun@users.noreply.github.com>
2023-11-15 19:20:33 +0900

    Update app3.py

--- app3.py
+++ app3.py
@@ -14,20 +14,5 @@ from feature_extraction import feature_extract
 from feature_extraction import feature_extract2
 
 
-# 각 페이지에 대한 내용 표시
-# 사이드바에 링크 추가
-st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Home", "About", "Contact"])
-
-# 각 페이지에 대한 내용 표시
-if page == "Home":
-    st.title("Home Page")
-    st.write("Welcome to the Home Page.")
-elif page == "About":
-    st.title("About Page")
-    st.write("This is the About Page.")
-elif page == "Contact":
-    st.title("Contact Page")
-    st.write("You can contact us here.")
 
    
