a15217fa69717ac903c26b0ca16278447b792bab
sueeun <83956730+sueeun@users.noreply.github.com>
2023-11-15 22:44:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -21,8 +21,28 @@ page = st.sidebar.radio("Go to", ["Home", "About", "Contact"])
 
 # 각 페이지에 대한 내용 표시
 if page == "Home":
-    st.title("Home Page")
-    st.write("Welcome to the Home Page.")
+    st.title('로그 데이터 처리 앱')
+
+    # 파일 업로드
+    uploaded_file = st.file_uploader("CSV 파일 선택", type="csv")
+
+    if uploaded_file is not None:
+        # CSV 파일 읽기
+        log_df = pd.read_csv(uploaded_file)
+
+        # 로그 데이터 처리
+        processed_log_df = process_log_data(log_df)
+
+        # 처리된 데이터 표시
+        st.write("처리된 로그 데이터:")
+        st.write(processed_log_df)
+
+        # 처리된 데이터를 새로운 CSV 파일로 저장
+        processed_file_path = 'processed_file.csv'
+        processed_log_df.to_csv(processed_file_path, index=False)
+
+        # 처리된 파일을 다운로드할 수 있는 링크 제공
+        st.markdown(f"처리된 데이터 다운로드: [처리된 파일]({processed_file_path})")
 elif page == "About":
     st.title("About Page")
     st.write("This is the About Page.")
