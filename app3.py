cb6b71ce327116204d48cd594dd8753980591b7e
sueeun <83956730+sueeun@users.noreply.github.com>
2023-11-16 18:06:30 +0900

    Update app3.py

--- app3.py
+++ app3.py
@@ -101,8 +101,10 @@ def main():
    dbscan_value_counts = df_entity['cluster_dbscan'].value_counts()
    x = np.arange(2)
 
-   result = [dbscan_value_counts.index.values[0],dbscan_value_counts.index.values[1:]]
-   count = [dbscan_value_counts.values[0],dbscan_value_counts.values[1:].sum()]
+   # result = [dbscan_value_counts.index.values[0],dbscan_value_counts.index.values[1:]]
+   # count = [dbscan_value_counts.values[0],dbscan_value_counts.values[1:].sum()]
+   result = dbscan_value_counts.index.values
+   count = dbscan_value_counts.values
 
    fig_dbscan_bar = plt.figure(figsize=(10, 6))
    plt.bar(x, count)
