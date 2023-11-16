87c4fff950ef738f5f2df2d93f13bcf8ab3c9931
sueeun <83956730+sueeun@users.noreply.github.com>
2023-11-16 14:13:41 +0900

    Update process_log.py

--- process_log.py
+++ process_log.py
@@ -7,7 +7,7 @@ def process_log_data(log_df):
     log_df['Timestamp'] = log_df['message'].str.extract(r'(\d+/\w+/\d+\d+:\d+:\d+:\d+)')
     log_df['Timestamp'] = pd.to_datetime(log_df['Timestamp'], format='%d/%b/%Y:%H:%M:%S').dt.strftime('%Y-%m-%d %H:%M:%S')
     log_df['Host'] = log_df['message'].str.extract(r'(\d+.\d+.\d+.\d+)')
-    log_df[['Method', 'Path']] = log_df['message'].str.extract(r'(HEAD|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH|POST|GET)\s+(.?)\s+HTTP')
+    log_df[['Method', 'Path']] = log_df['message'].str.extract(r'(HEAD|PUT|DELETE|CONNECT|OPTIONS|TRACE|PATCH|POST|GET)\s+([^\s]+)\s+HTTP')
     log_df['Protocol'] = log_df['message'].str.extract(r'(HTTP/\d+.\d+)')
     log_df['Status'] = log_df['message'].str.extract(r'(\d+)\s+\d+')
     log_df['Bytes'] = log_df['message'].str.extract(r'\d+\s+(\d+)')
