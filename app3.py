cc4ba8dcd9eba45a2cac3b5bb552a26c524c1d91
sueeun <83956730+sueeun@users.noreply.github.com>
2023-11-16 17:05:26 +0900

    Update app3.py

--- app3.py
+++ app3.py
@@ -27,15 +27,24 @@ def main():
    df_entity[columns_to_scale] = scaler.transform(df_entity[columns_to_scale])
 
    cols_to_train = ['method_cnt','method_post','protocol_1_0','status_major','status_404','status_499','status_cnt','path_same','path_xmlrpc','ua_cnt','has_payload','bytes_avg','bytes_std']
-   kmeans = KMeans (n_clusters=2, random_state=42)
-   # 정상/ 비정상 클러스터로 나누어 보기
-   kmeans.fit(df_entity[cols_to_train])
 
-   df_entity['cluster_kmeans'] = kmeans.predict(df_entity[cols_to_train])
-   # st.write(df_entity)
+  # Kmeans
+  kmeans = KMeans (n_clusters=2, random_state=42)
+  kmeans.fit(df_entity[cols_to_train])
+  df_entity['cluster_kmeans'] = kmeans.predict(df_entity[cols_to_train])
+
+  # DBSCAN
+  dbscan = DBSCAN(eps=0.5,min_samples=2)
+  dbscan.fit(df_entity[cols_to_train])
+  df_entity['cluster_dbscan'] = dbscan.fit_predict(df_entity[cols_to_train])
+
+   # st.write(df_entity
+
    st.title('colab 코드 띄우기')
    st.write(df_entity['cluster_kmeans'].value_counts())
    st.write(df_entity[df_entity['cluster_kmeans']==0].index)
+
+   st.title('PCA 그래프')
 
    # PCA를 사용하여 데이터의 차원을 2로 축소
    pca = PCA(n_components=2)
@@ -45,16 +54,26 @@ def main():
    df_entity['pca_1'] = pca_result[:, 0]
    df_entity['pca_2'] = pca_result[:, 1]
 
-   st.title('PCA 그래프')
    # 2D PCA 결과를 시각화
-   fig = plt.figure(figsize=(10, 6))
+   fig_kmeans = plt.figure(figsize=(10, 6))
    plt.scatter(df_entity['pca_1'], df_entity['pca_2'], c=df_entity['cluster_kmeans'], cmap='viridis', s=60)
    plt.xlabel("PCA 1")
    plt.ylabel("PCA 2")
-   plt.title("전체 Feature를 이용한 이상탐지된 Entity 시각화 (PCA 결과)")
+   plt.title("KMeans 클러스터링된 Entity 시각화 (PCA 결과)")
    plt.colorbar(label='클러스터')
+   
+   st.pyplot(fig_kmeans)
 
-   st.pyplot(fig)
+   # 2D PCA 결과를 시각화
+   fig_dbscan = plt.figure(figsize=(10, 6))
+   plt.scatter(df_entity['pca_1'], df_entity['pca_2'], c=df_entity['cluster_dbscan'], cmap='viridis', s=60)
+   plt.xlabel("PCA 1")
+   plt.ylabel("PCA 2")
+   plt.title("DBSCAN 클러스터링된 Entity 시각화 (PCA 결과)")
+   plt.colorbar(label='클러스터')
+   
+   st.pyplot(fig_dbscan)
+   
 
 
    # -- 막대 그래프 --
