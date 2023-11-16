0581c3dd0cd8822285f35d3d02c995b2c3068ff8
sueeun <83956730+sueeun@users.noreply.github.com>
2023-11-16 17:27:29 +0900

    Update app3.py

--- app3.py
+++ app3.py
@@ -80,43 +80,43 @@ def main():
    # -- 막대 그래프 --
    # st.title('막대그래프')
    
-   # # Kmeans
-   # kmans_value_counts = df_entity['cluster_kmeans'].value_counts()
-   # x = np.arange(2)
+   # Kmeans
+   kmans_value_counts = df_entity['cluster_kmeans'].value_counts()
+   x = np.arange(2)
    
-   # result = kmans_value_counts.index.values
-   # count = kmans_value_counts.values
+   result = kmans_value_counts.index.values
+   count = kmans_value_counts.values
    
-   # fig_kmeans_bar = plt.figure(figsize=(10, 6))
-   # plt.bar(x, count)
-   # plt.set_xticks(x)
-   # plt.set_xticklabels(result)
+   fig_kmeans_bar = plt.figure(figsize=(10, 6))
+   plt.bar(x, count)
+   plt.set_xticks(x)
+   plt.set_xticklabels(result)
    
-   # for i, value in enumerate(result):
-   #     plt.text(x[i], count[i], count[i], ha='center', va='bottom')
+   for i, value in enumerate(result):
+       plt.text(x[i], count[i], count[i], ha='center', va='bottom')
 
-   # st.pyplot(fig_kmeans_bar)
+   st.pyplot(fig_kmeans_bar)
    
-   # # DBSCAN
-   # dbscan_value_counts = df_entity['cluster_dbscan'].value_counts()
-   # x = np.arange(2)
+   # DBSCAN
+   dbscan_value_counts = df_entity['cluster_dbscan'].value_counts()
+   x = np.arange(2)
 
-   # result = [dbscan_value_counts.index.values[0],dbscan_value_counts.index.values[1:]]
-   # count = [dbscan_value_counts.values[0],dbscan_value_counts.values[1:].sum()]
+   result = [dbscan_value_counts.index.values[0],dbscan_value_counts.index.values[1:]]
+   count = [dbscan_value_counts.values[0],dbscan_value_counts.values[1:].sum()]
 
-   # fig_dbscan_bar = plt.figure(figsize=(10, 6))
-   # plt.bar(x, count)
-   # plt.xticks(x, result)
+   fig_dbscan_bar = plt.figure(figsize=(10, 6))
+   plt.bar(x, count)
+   plt.xticks(x, result)
    
-   # for i, value in enumerate(result):
-   #   plt.text(x[i], count[i], count[i], ha='center', va='bottom')
+   for i, value in enumerate(result):
+     plt.text(x[i], count[i], count[i], ha='center', va='bottom')
    
-   # st.pyplot(fig_dbscan_bar)
+   st.pyplot(fig_dbscan_bar)
 
    # 아이피 띄우기
-   st.title('이상탐지된 아이피')
-   df_entity[df_entity['cluster_kmeans']!=1].index
-   df_entity[df_entity['cluster_dbscan']!=0].index
+   # st.title('이상탐지된 아이피')
+   # df_entity[df_entity['cluster_kmeans']!=1].index
+   # df_entity[df_entity['cluster_dbscan']!=0].index
    
    
 if __name__ == '__main__':
