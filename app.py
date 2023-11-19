3a26e2887c118e12b91d8a4d34a82b82e5f394e8
sueeun <83956730+sueeun@users.noreply.github.com>
2023-11-19 22:11:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ elif page == "2.ㅤFeature extract":
     processed_file = st.file_uploader("CSV 파일 선택(1)", type="csv")
     
     if processed_file is not None:
-        st.write(processed_file)
+        st.write(pd.read_csv(log_file))
 
     elif processed_file is not None:
         # CSV 파일 읽기
